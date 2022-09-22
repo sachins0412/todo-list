ce21d36668e295066e326ba824b9b6256058271d
sachins0412 <saxenas964@gmail.com>
2022-09-22 16:23:16 +0530

    add then & catch to connection

--- src/database/mongoose.js
+++ src/database/mongoose.js
@@ -2,4 +2,7 @@ const mongoose = require("mongoose");
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-mongoose.connect(MONGODB_URI);
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => console.log("mongodb connected"))
+  .catch((e) => console.log(e));
