4bf71f36ac26e15fde2371e327e88a560c448ced
sachins0412 <saxenas964@gmail.com>
2022-09-22 16:39:06 +0530

    email validation

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = mongoose.Schema({
   name: {
@@ -13,6 +14,11 @@ const userSchema = mongoose.Schema({
     trim: true,
     lowercase: true,
     unique: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("email not valid");
+      }
+    },
   },
   password: {
     type: String,
