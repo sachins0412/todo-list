8d9b5086c43739893b0fc4fc3fc14f9e5c2caeea
sachins0412 <saxenas964@gmail.com>
2022-09-22 16:42:27 +0530

    encrypt user passwords before saving

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = mongoose.Schema({
   name: {
@@ -26,6 +27,16 @@ const userSchema = mongoose.Schema({
     trim: true,
     minLength: 8,
   },
+});
+
+userSchema.pre("save", async function (next) {
+  const user = this;
+
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+
+  next();
 });
 
 const User = mongoose.model("User", userSchema);
