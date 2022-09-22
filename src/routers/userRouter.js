53aaab10333d697e490c3c2cf80249bd6e3b2694
sachins0412 <saxenas964@gmail.com>
2022-09-22 16:36:56 +0530

    add user signin router

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/users", (req, res) => {
-  res.send("respones from userRouter");
+const { User } = require("./../models/user");
+
+router.post("/users", async (req, res) => {
+  const user = new User(req.body);
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch (e) {
+    res.status(400).send(e.message);
+  }
 });
 
 module.exports = router;
