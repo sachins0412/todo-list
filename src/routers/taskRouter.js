51ede8b563910acd325eb7a098b07335575c3bf3
sachins0412 <saxenas964@gmail.com>
2022-09-22 17:45:03 +0530

    add route for getTasks

--- src/routers/taskRouter.js
+++ src/routers/taskRouter.js
@@ -42,4 +42,26 @@ router.delete("/tasks/:id", async (req, res) => {
     res.status(400).send(e.message);
   }
 });
+
+router.get("/tasks/:id", async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+
+    if (!task) return res.status(404).send("task not found");
+
+    res.send(task);
+  } catch (e) {
+    res.status(400).send(e.message);
+  }
+});
+
+router.get("/tasks", async (req, res) => {
+  try {
+    const tasks = await Task.find();
+
+    res.send(tasks);
+  } catch (e) {
+    res.status(400).send(e.message);
+  }
+});
 module.exports = router;
