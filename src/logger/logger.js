022b1c710a06aa157177709b2ccf23ffc41d304a
sachins0412 <saxenas964@gmail.com>
2022-09-24 13:34:41 +0530

    create separate log file for different days

--- src/logger/logger.js
+++ src/logger/logger.js
@@ -1,10 +1,11 @@
 const { createLogger, format, transports } = require("winston");
 const { combine, timestamp, json } = format;
 
+let today = new Date().toISOString().split("T")[0];
 const logger = createLogger({
   level: process.env.APPLOG_LEVEL,
   format: combine(timestamp(), json()),
-  transports: new transports.File({ filename: "logs/app.log" }),
+  transports: new transports.File({ filename: `logs/app-${today}.log` }),
 });
 
 module.exports = logger;
